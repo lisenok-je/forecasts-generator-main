6a36c7b41ad58560c3f2c9053a2b848991a56d8a
lisenok-je <131145467+lisenok-je@users.noreply.github.com>
2024-01-24 10:28:12 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+let predictions = ["Со всеми трудностями ты легко справишься", "Пусть все мечты превращаются в реальность", "Если не сейчас, то когда?", "Попробуй!", "Мечтай масштабно"];
+
+function helperGeneration(min, max) {
+    const number = Math.random() * (max - min) + min;
+    return number;
+}
+const percent = parseInt(helperGeneration(0, 100));
+const predictionRandom = Math.floor(Math.random() * (predictions.length - 0) + 0);
+const predictionCard = predictions[predictionRandom];
+
+const button = document.querySelector('.forecast-btn');
+const item = document.querySelector('.forecast');
+const currentForecast = document.querySelector('#forest-item');
+
+
+button.addEventListener('click', () => {
+    const prediction = document.querySelector('h1');
+    const variations = document.querySelector('p');
+    prediction.textContent = predictionCard;
+    variations.textContent = 'Вероятность:' + percent + '%';
+    const card = predictionMap(prediction, variations);
+    item.prepend(card);
+
+    function predictionMap(prediction, variations) {
+        const newCard = currentForecast.cloneNode(true);
+        newCard.querySelector('h3').textContent = prediction.textContent;
+        newCard.querySelector('p').textContent = variations.textContent;
+      return newCard;
+    };
+});
+
